d6dd265e82b247c2e8caa6875f7d0dff3bd98f9e
Amin Aarab <amin.aarab@chasacademy.se>
2019-02-13 23:07:30 +0100

    Deleted recipe component and created a recipe service instead.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { BoomifyComponent } from './boomify/boomify.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { SavedComponent } from './saved/saved.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipes-details/recipes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoomifyComponent,
     RecipesComponent,
-    SavedComponent
+    SavedComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipes-list/recipes-list.component.spec.ts
+++ src/app/recipes-list/recipes-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BoomifyComponent } from './boomify.component';
+import { RecipesListComponent } from './recipes-list.component';
 
-describe('BoomifyComponent', () => {
-  let component: BoomifyComponent;
-  let fixture: ComponentFixture<BoomifyComponent>;
+describe('RecipesListComponent', () => {
+  let component: RecipesListComponent;
+  let fixture: ComponentFixture<RecipesListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoomifyComponent ]
+      declarations: [ RecipesListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BoomifyComponent);
+    fixture = TestBed.createComponent(RecipesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
