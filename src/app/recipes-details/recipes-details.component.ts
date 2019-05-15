e07a78b140e7e3ea369230fbe30fe7e083e2fe92
Amin Aarab <amin.aarab@chasacademy.se>
2019-05-15 18:52:11 +0200

    added CURD operation for saving recipes

--- src/app/recipes-details/recipes-details.component.ts
+++ src/app/recipes-details/recipes-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import {RecipeService } from '../recipe.service';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-recipes-details',
@@ -14,6 +15,7 @@ export class RecipesDetailsComponent implements OnInit {
   constructor(
     private recipeService: RecipeService,
     private route: ActivatedRoute,
+    private userService: UsersService,
   ) {}
 
 ngOnInit() {
@@ -24,6 +26,35 @@ ngOnInit() {
   });
 }
 
+saveRecipe() {
+  console.log("heloo!")
 
+  let saveDbModel = this.constructModel(this.recipe[0])
+
+  this.userService.sendRecipe(saveDbModel).subscribe(data => {
+
+  })
+}
+
+arrayToString(array) {
+  let string = "";
+  array.forEach(e => {
+    string = string + e + ",";
+  });
+  return string;
+}
+
+constructModel(recipe) {
+  let saveDbModel = {
+
+    // email: this.token.getEmail(),
+    
+    title : this.recipe.label,
+    description : this.arrayToString(this.recipe.ingredientLines),
+    
+  }
+
+  return saveDbModel;
+}
 
 }
